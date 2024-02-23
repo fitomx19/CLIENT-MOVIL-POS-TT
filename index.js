0d5a405374e55a175089abce00da8880815ad57c
Adolfo MAC <m.adolfohuerta@gmail.com>
2024-02-22 22:13:39 -0600

    Ya jala con esto --> npx react-native run-android     , probare generarlo en android studio

---
+++ index.js
@@ -0,0 +1,6 @@
+import { registerRootComponent } from 'expo';
+import { AppRegistry } from 'react-native';
+import App from './App'; // Importa el componente App desde App.js
+
+// Registra el componente App
+registerRootComponent(App);
